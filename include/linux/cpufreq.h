affc5a0cbe6fa20cf65bcbccac5b2f5ac2b737fb
GameTheory <game.theory7@hotmail.com>
2018-12-05 19:28:31 -0400

    Added ondemandplus governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -453,6 +453,9 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
 extern struct cpufreq_governor cpufreq_gov_darkness;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
+extern struct cpufreq_governor cpufreq_gov_ondemandplus;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandplus)
 #endif
 
 /*********************************************************************
