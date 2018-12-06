871dad0cda2b0130698ba47f6aba1b6320dab47c
GameTheory <game.theory7@hotmail.com>
2018-12-05 20:42:48 -0400

    Added ironactive governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -462,6 +462,9 @@ extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
 extern struct cpufreq_governor cpufreq_gov_yankactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IRONACTIVE)
+extern struct cpufreq_governor cpufreq_gov_ironactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ironactive)
 #endif
 
 /*********************************************************************
